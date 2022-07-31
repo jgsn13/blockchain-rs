ab540369f1ef71f074b88078fc9edcc17e659a65
Joaquim Gregório da Silva Neto <joaquim.ibi@gmail.com>
2022-07-31 16:29:19 -0300

    feat: create sample block

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use blockchainlib::*;
 
 fn main() {
-    println!("Hello, world!");
+    let block = Block::new(13, now(), vec![0; 32], 0, "Initial Block!".to_owned());
+
+    println!("{:#?}", &block);
 }
